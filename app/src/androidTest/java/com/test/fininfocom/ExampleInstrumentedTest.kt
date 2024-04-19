d8d8fcf82612fbde42bfcd64df264cd596a18e71
ArjunBS <arjun.bs@invensfot.co.in>
2024-04-19 15:36:50 +0530

    icon image removed

--- app/src/androidTest/java/com/test/fininfocom/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/test/fininfocom/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.xbsda.fininfocom", appContext.packageName)
+        assertEquals("com.test.fininfocom", appContext.packageName)
     }
 }
